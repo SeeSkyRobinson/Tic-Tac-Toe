3fa1a2f2b6d0e1aea1860ad4fd952e3137190dc2
SeeSkyRobinson <cskyrobinson@gmail.com>
2021-08-25 13:02:16 +1000

    start first project

---
+++ app.js
@@ -0,0 +1,17 @@
+//Making a tic tac to game
+
+//draw out the grid
+    //grid of 9 divs
+    //with borders on some of the sides to make the cross hatch shape
+
+//click on a space will fill x
+    //adds content to the div
+//next click will fill o
+    //adds content to the div
+//loops between clicks for x and o each time checking if three in a row occurs
+//if win condition occurs, print x or o wins, updates score counter
+//empties fill from the board
+
+
+//make it fancy later
+
